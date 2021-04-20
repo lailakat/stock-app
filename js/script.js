4097abe920e9674392c1acda959be7701710b787
lailakat <laila.katumba@boohoo.com>
2021-04-20 18:06:27 +0100

    populate data and change styling

---
+++ js/script.js
@@ -0,0 +1,101 @@
+var myArray = [
+    {
+        'sku': 'AAA12345-123-123',
+        'physicalWarehouse': 'Wellingborough',
+        'logicalWarehouse': 'Coast',
+        'skuDescription': 'A pretty dress',
+        'availableToSell': '50',
+        'physicalStock': 'X'
+    }, 
+    {
+        'sku': 'AAA12345-123-123',
+        'physicalWarehouse': 'Wellingborough',
+        'logicalWarehouse': 'Coast',
+        'skuDescription': 'A pretty dress',
+        'availableToSell': '50',
+        'physicalStock': 'X'
+    }, 
+    {
+        'sku': 'AAA12345-123-123',
+        'physicalWarehouse': 'Wellingborough',
+        'logicalWarehouse': 'Coast',
+        'skuDescription': 'A pretty dress',
+        'availableToSell': '50',
+        'physicalStock': 'X'
+    }, 
+    {
+        'sku': 'AAA12345-123-123',
+        'physicalWarehouse': 'Wellingborough',
+        'logicalWarehouse': 'Coast',
+        'skuDescription': 'A pretty dress',
+        'availableToSell': '50',
+        'physicalStock': 'X'
+    }, 
+    {      'sku': 'AAA12345-123-123',
+    'physicalWarehouse': 'Wellingborough',
+    'logicalWarehouse': 'Coast',
+    'skuDescription': 'A pretty dress',
+    'availableToSell': '50',
+    'physicalStock': 'X'
+    },
+    {
+        'sku': 'AAA12345-123-123',
+        'physicalWarehouse': 'Wellingborough',
+        'logicalWarehouse': 'Coast',
+        'skuDescription': 'A pretty dress',
+        'availableToSell': '50',
+        'physicalStock': 'X'
+    },
+    {
+        'sku': 'AAA12345-123-123',
+        'physicalWarehouse': 'Wellingborough',
+        'logicalWarehouse': 'Coast',
+        'skuDescription': 'A pretty dress',
+        'availableToSell': '50',
+        'physicalStock': 'X'
+    },
+    {
+        'sku': 'AAA12345-123-123',
+        'physicalWarehouse': 'Wellingborough',
+        'logicalWarehouse': 'Coast',
+        'skuDescription': 'A pretty dress',
+        'availableToSell': '50',
+        'physicalStock': 'X'
+    },
+    {
+        'sku': 'AAA12345-123-123',
+        'physicalWarehouse': 'Wellingborough',
+        'logicalWarehouse': 'Coast',
+        'skuDescription': 'A pretty dress',
+        'availableToSell': '50',
+        'physicalStock': 'X'
+    },
+    {
+        'sku': 'AAA12345-123-123',
+        'physicalWarehouse': 'Wellingborough',
+        'logicalWarehouse': 'Coast',
+        'skuDescription': 'A pretty dress',
+        'availableToSell': '50',
+        'physicalStock': 'X'
+    }
+]
+
+buildTable(myArray)
+
+function buildTable(data) {
+    let table = document.getElementById('myTable');
+
+    for (let i = 0; i < data.length; i++ ) {
+        let row = `<tr>
+                        <td>${data[i].sku}</td>
+                        <td>${data[i].physicalWarehouse}</td>
+                        <td>${data[i].logicalWarehouse}</td>
+                        <td>${data[i].skuDescription}</td>
+                        <td>${data[i].availableToSell}</td>
+                        <td>${data[i].physicalStock}</td>
+                    </tr>`
+
+                    table.innerHTML += row
+    }
+}
+
